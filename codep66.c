f531760d35c05ec1f7c542da24e829d49156b75a
HarishmaKrishnakumar <46838369+HarishmaKrishnakumar@users.noreply.github.com>
2019-03-11 04:51:01 +0530

    Update codep66.c

--- codep66.c
+++ codep66.c
@@ -2,39 +2,28 @@
 
 int main()
 {
-    int N,j,i,c,K,t=0;
-    scanf("%d%d",&N,&K);
-    int a[N],b[N];
-    for(i=0;i<N;i++)
-    {
-        scanf("%d",&b[i]);
-    }
-    c=0;
-    t=0;
+    int N,i,c=0,j,K;
+    scanf("%d %d",&N,&K);
+   int a[N];
+	for(i=0;i<N;i++)
+	{
+		scanf("%d",&a[i]);
+	}
 for(i=0;i<N;i++)
-{
-    for(j=i+1;j<N;j++)
-    {
-        if(b[i]==b[j])
-        {
-         c++;  
-        t=1;
-         b[j]='\0';
-         
-        }
-    }
-    if(t==0)
-    {
-     b[i]='\0';
-    }
-    
-}
-
-for(i=0;i<c;i++)
-{
-    if(b[i]!='\0' && c==K)
-    printf("%d",b[i]);
-}
-
+	{
+		for(j=i+1;j<N;j++)
+		{
+			if(a[i]==a[j])
+			{
+				c=c+1;
+				
+			}
+		}
+		if(c==K)
+		{
+			printf("%d",a[i]);
+			break;
+		}
+	}
     return 0;
 }
