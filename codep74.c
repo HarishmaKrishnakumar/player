8faafe0133a43e30b17fff6d3e5d4d20937e28e9
HarishmaKrishnakumar <46838369+HarishmaKrishnakumar@users.noreply.github.com>
2019-03-11 03:26:25 +0530

    Create codep74.c

---
+++ codep74.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int A,c=1,B,i;
+    scanf("%d%d",&A,&B);
+    
+    for(i=1;i<=B;i++)
+ {
+     c=c*A;
+     }
+    printf("%d",c);
+   
+  
+    return 0;
+}
