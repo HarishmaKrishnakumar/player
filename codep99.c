734f76269fbaa6e7beee8ffe946c137b9ea0c531
HarishmaKrishnakumar <46838369+HarishmaKrishnakumar@users.noreply.github.com>
2019-03-11 04:07:56 +0530

    Create codep99.c

---
+++ codep99.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    long N,r,t=1,bin=0;
+   scanf("%ld",&N);
+   
+   while(N)
+   {
+       
+       r=N%10;
+       N=N/10;
+       bin=bin+(r*t);
+      t=t*2;
+      
+   }
+   printf("%o",bin);
+    return 0;
+}
