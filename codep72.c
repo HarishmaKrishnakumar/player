a555e054007b7931d6b690702cce4ef455d803a6
HarishmaKrishnakumar <46838369+HarishmaKrishnakumar@users.noreply.github.com>
2019-03-11 05:05:13 +0530

    Create codep72.c

---
+++ codep72.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int N,i,j;
+	scanf("%d",&N);
+	int a[N];
+
+	for(i=0;i<N;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<N;i++)
+	{
+		if(a[i+1]<a[i])
+		{
+			printf("%d",a[i]);
+			break;
+		}
+	
+	}
+    return 0;
+}
